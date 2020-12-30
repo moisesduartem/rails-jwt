88045e05e9b2da509639248fc55484619b52d770
Moisés Mariano <moisesduartem@gmail.com>
2020-12-30 20:56:21 -0300

    routes settings

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+  path: 'auth',
+  path_names: {
+  sign_in: 'login',
+  sign_out: 'logout',
+  registration: 'register'
+  },
+  controllers: {
+  sessions: 'auth/sessions',
+  registrations: 'auth/registrations'
+  }  
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
